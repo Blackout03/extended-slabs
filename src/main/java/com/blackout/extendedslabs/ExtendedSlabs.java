1cf11b111af9259f7619b29287f3ecb3bbdb5978
Blackout03 <gardner.hayden03@gmail.com>
2022-06-10 20:03:12 +1000

    2.1.0 | Fix Stuff. Add Stuff.

--- src/main/java/com/blackout/extendedslabs/ExtendedSlabs.java
+++ src/main/java/com/blackout/extendedslabs/ExtendedSlabs.java
@@ -6,67 +6,79 @@ import com.blackout.extendedslabs.init.ESPSlabs;
 import com.blackout.extendedslabs.init.ESPStairs;
 import com.blackout.extendedslabs.init.ESPVerticalSlabs;
 import com.blackout.extendedslabs.render.block.BlockRenderLayer;
-import com.blackout.extendedslabs.util.CreativeTab;
+import com.blackout.extendedslabs.util.ESPCreativeModeTab;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.world.item.CreativeModeTab;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModContainer;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.forgespi.language.IModInfo;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.maven.artifact.versioning.ArtifactVersion;
+
+import java.util.Optional;
 
 @Mod(ExtendedSlabs.MODID)
 public class ExtendedSlabs {
-    public static final String MODID = "extendedslabs";
-    public static final String MODNAME = "Extended Slabs";
-    public static final String VERSION = "2.0.0";
+	public static final String MODID = "extendedslabs";
+	public static final String MODNAME = "Extended Slabs";
+	public static ArtifactVersion VERSION = null;
+	public static final Logger LOGGER = LogManager.getLogger();
 
-    public static final Logger LOGGER = LogManager.getLogger(MODID);
-    public static final CreativeModeTab GROUP = new CreativeTab();
+	public static final CreativeModeTab GROUP = new ESPCreativeModeTab();
 
-    public ExtendedSlabs() {
-        LOGGER.debug(MODNAME + " Version is:" + VERSION);
+	public ExtendedSlabs() {
+		Optional<? extends ModContainer> opt = ModList.get().getModContainerById(MODID);
+		if (opt.isPresent()) {
+			IModInfo modInfo = opt.get().getModInfo();
+			VERSION = modInfo.getVersion();
+		} else LOGGER.warn("Cannot get version from mod info");
 
-        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		LOGGER.debug(MODNAME + " Version is: " + VERSION);
+		LOGGER.debug("Mod ID for " + MODNAME + " is: " + MODID);
 
-        eventBus.addListener(this::gatherData);
+		IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ESPSlabs.ITEMS.register(eventBus);
-        ESPSlabs.BLOCKS.register(eventBus);
-        ESPStairs.ITEMS.register(eventBus);
-        ESPStairs.BLOCKS.register(eventBus);
-        ESPCorners.ITEMS.register(eventBus);
-        ESPCorners.BLOCKS.register(eventBus);
-        ESPVerticalSlabs.ITEMS.register(eventBus);
-        ESPVerticalSlabs.BLOCKS.register(eventBus);
-        MinecraftForge.EVENT_BUS.register(this);
+		eventBus.addListener(this::gatherData);
 
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
-    }
+		ESPSlabs.ITEMS.register(eventBus);
+		ESPSlabs.BLOCKS.register(eventBus);
+		ESPStairs.ITEMS.register(eventBus);
+		ESPStairs.BLOCKS.register(eventBus);
+		ESPCorners.ITEMS.register(eventBus);
+		ESPCorners.BLOCKS.register(eventBus);
+		ESPVerticalSlabs.ITEMS.register(eventBus);
+		ESPVerticalSlabs.BLOCKS.register(eventBus);
+		MinecraftForge.EVENT_BUS.register(this);
 
-    private void gatherData(final GatherDataEvent event) {
-        DataGenerator dataGenerator = event.getGenerator();
-        final ExistingFileHelper existing = event.getExistingFileHelper();
+		FMLJavaModLoadingContext.get().getModEventBus().addListener(this::clientSetup);
+	}
 
-        if (event.includeServer()) {
-            dataGenerator.addProvider(new ESPLootTableProvider(dataGenerator));
-            dataGenerator.addProvider(new ESPBlockModelProvider(dataGenerator, MODID, existing));
-            dataGenerator.addProvider(new ESPItemModelGenerator(dataGenerator, existing));
-            dataGenerator.addProvider(new ESPBlockStateProvider(dataGenerator, MODID, existing));
-            dataGenerator.addProvider(new ESPBlockTagsProvider(dataGenerator, existing));
-            dataGenerator.addProvider(new ESPStairRecipeProvider(dataGenerator));
-            dataGenerator.addProvider(new ESPCornerRecipeProvider(dataGenerator));
-            dataGenerator.addProvider(new ESPSlabRecipeProvider(dataGenerator));
-            dataGenerator.addProvider(new ESPVerticalSlabRecipeProvider(dataGenerator));
-        }
-    }
+	private void gatherData(final GatherDataEvent event) {
+		DataGenerator dataGenerator = event.getGenerator();
+		final ExistingFileHelper existing = event.getExistingFileHelper();
 
-    private void clientSetup(final FMLClientSetupEvent event) {
-        BlockRenderLayer.renderBlock();
-        LOGGER.info("renderBlock");
-    }
+		if (event.includeServer()) {
+			dataGenerator.addProvider(new ESPLootTableProvider(dataGenerator));
+			dataGenerator.addProvider(new ESPBlockModelProvider(dataGenerator, MODID, existing));
+			dataGenerator.addProvider(new ESPItemModelGenerator(dataGenerator, existing));
+			dataGenerator.addProvider(new ESPBlockStateProvider(dataGenerator, MODID, existing));
+			dataGenerator.addProvider(new ESPBlockTagsProvider(dataGenerator, existing));
+			dataGenerator.addProvider(new ESPStairRecipeProvider(dataGenerator));
+			dataGenerator.addProvider(new ESPCornerRecipeProvider(dataGenerator));
+			dataGenerator.addProvider(new ESPSlabRecipeProvider(dataGenerator));
+			dataGenerator.addProvider(new ESPVerticalSlabRecipeProvider(dataGenerator));
+		}
+	}
+
+	private void clientSetup(final FMLClientSetupEvent event) {
+		BlockRenderLayer.renderBlock();
+	}
 }
--- src/main/java/com/blackout/extendedslabs/blocks/copper/WeatheringCopperVerticalSlabBlock.java
+++ src/main/java/com/blackout/extendedslabs/blocks/copper/WeatheringCopperVerticalSlabBlock.java
@@ -1,21 +1,50 @@
 package com.blackout.extendedslabs.blocks.copper;
 
 import com.blackout.extendedslabs.blocks.VerticalSlabBlock;
+import com.blackout.extendedslabs.util.CopperStateMap;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.WeatheringCopper;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.phys.BlockHitResult;
 import org.jetbrains.annotations.NotNull;
 
+import java.util.Optional;
 import java.util.Random;
 
 public class WeatheringCopperVerticalSlabBlock extends VerticalSlabBlock implements WeatheringCopper {
     private final WeatheringCopper.WeatherState weatherState;
 
-    public WeatheringCopperVerticalSlabBlock(WeatheringCopper.WeatherState weatherState, BlockBehaviour.Properties properties) {
+    public WeatheringCopperVerticalSlabBlock(BlockBehaviour.Properties properties, WeatheringCopper.WeatherState weatherState) {
         super(properties);
         this.weatherState = weatherState;
+    }
+
+    @Override
+    public @NotNull InteractionResult use(@NotNull BlockState state, @NotNull Level level, @NotNull BlockPos pos, Player player, @NotNull InteractionHand hand, @NotNull BlockHitResult hitResult) {
+        var stack = player.getItemInHand(hand);
+        if (stack.getItem() == Items.HONEYCOMB) {
+            var block = CopperStateMap.getWaxed(state.getBlock());
+            if (block.isPresent()) {
+                level.setBlock(pos, block.map(b -> b.withPropertiesOf(state)).get(), 11);
+                level.levelEvent(player, 3003, pos, 0);
+                stack.shrink(1);
+                return InteractionResult.SUCCESS;
+            }
+        }
+        return InteractionResult.PASS;
+    }
+
+
+    @Override
+    public @NotNull Optional<BlockState> getNext(BlockState state) {
+        return CopperStateMap.getIncrease(state.getBlock()).map((block) -> block.withPropertiesOf(state));
     }
 
     @SuppressWarnings("deprecation")
@@ -24,10 +53,11 @@ public class WeatheringCopperVerticalSlabBlock extends VerticalSlabBlock impleme
     }
 
     public boolean isRandomlyTicking(BlockState state) {
-        return WeatheringCopper.getNext(state.getBlock()).isPresent();
+        return CopperStateMap.getIncrease(state.getBlock()).isPresent();
     }
 
-    public WeatheringCopper.@NotNull WeatherState getAge() {
+    @Override
+    public @NotNull WeatherState getAge() {
         return this.weatherState;
     }
 }
