9667e8c26f37028e93b162445ba1148e33883797
Blackout03 <gardner.hayden03@gmail.com>
2022-09-08 20:01:32 +1000

    Commit this

--- src/main/java/com/blackout/extendedslabs/datagen/ESPBlockTagsProvider.java
+++ src/main/java/com/blackout/extendedslabs/datagen/ESPBlockTagsProvider.java
@@ -1,11 +1,9 @@
 package com.blackout.extendedslabs.datagen;
 
 import com.blackout.extendedslabs.ExtendedSlabs;
-import com.blackout.extendedslabs.init.ESPCorners;
 import com.blackout.extendedslabs.init.ESPMaterial;
 import com.blackout.extendedslabs.init.ESPSlabs;
 import com.blackout.extendedslabs.init.ESPStairs;
-import com.blackout.extendedslabs.init.ESPVerticalSlabs;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BlockTagsProvider;
 import net.minecraft.tags.BlockTags;
@@ -60,49 +58,5 @@ public class ESPBlockTagsProvider extends BlockTagsProvider {
 			ExtendedSlabs.LOGGER.info("Tag: " + BlockTags.STAIRS.location() + " Block: " + block.getId());
 			tag(BlockTags.STAIRS).add(block.get());
 		}
-
-		tag(BlockTags.MINEABLE_WITH_PICKAXE).add(ESPSlabs.TUFF_SLAB.get(), ESPSlabs.CALCITE_SLAB.get(),
-				ESPSlabs.DRIPSTONE_SLAB.get(), ESPSlabs.END_STONE_SLAB.get(),
-				ESPSlabs.NETHERRACK_SLAB.get(), ESPSlabs.CRIMSON_NYLIUM_SLAB.get(),
-				ESPSlabs.WARPED_NYLIUM_SLAB.get(), ESPSlabs.WHITE_CONCRETE_SLAB.get(),
-				ESPSlabs.ORANGE_CONCRETE_SLAB.get(), ESPSlabs.MAGENTA_CONCRETE_SLAB.get(),
-				ESPSlabs.LIGHT_BLUE_CONCRETE_SLAB.get(), ESPSlabs.YELLOW_CONCRETE_SLAB.get(),
-				ESPSlabs.LIME_CONCRETE_SLAB.get(), ESPSlabs.PINK_CONCRETE_SLAB.get(),
-				ESPSlabs.GRAY_CONCRETE_SLAB.get(), ESPSlabs.LIGHT_GRAY_CONCRETE_SLAB.get(),
-				ESPSlabs.CYAN_CONCRETE_SLAB.get(), ESPSlabs.PURPLE_CONCRETE_SLAB.get(),
-				ESPSlabs.BLUE_CONCRETE_SLAB.get(), ESPSlabs.BROWN_CONCRETE_SLAB.get(),
-				ESPSlabs.GREEN_CONCRETE_SLAB.get(), ESPSlabs.RED_CONCRETE_SLAB.get(),
-				ESPSlabs.BLACK_CONCRETE_SLAB.get(), ESPSlabs.TERRACOTTA_SLAB.get(),
-				ESPSlabs.WHITE_TERRACOTTA_SLAB.get(), ESPSlabs.ORANGE_TERRACOTTA_SLAB.get(),
-				ESPSlabs.MAGENTA_TERRACOTTA_SLAB.get(), ESPSlabs.LIGHT_BLUE_TERRACOTTA_SLAB.get(),
-				ESPSlabs.YELLOW_TERRACOTTA_SLAB.get(), ESPSlabs.LIME_TERRACOTTA_SLAB.get(),
-				ESPSlabs.PINK_TERRACOTTA_SLAB.get(), ESPSlabs.GRAY_TERRACOTTA_SLAB.get(),
-				ESPSlabs.LIGHT_GRAY_TERRACOTTA_SLAB.get(), ESPSlabs.CYAN_TERRACOTTA_SLAB.get(),
-				ESPSlabs.PURPLE_TERRACOTTA_SLAB.get(), ESPSlabs.BLUE_TERRACOTTA_SLAB.get(),
-				ESPSlabs.BROWN_TERRACOTTA_SLAB.get(), ESPSlabs.GREEN_TERRACOTTA_SLAB.get(),
-				ESPSlabs.RED_TERRACOTTA_SLAB.get(), ESPSlabs.BLACK_TERRACOTTA_SLAB.get(),
-				ESPSlabs.WHITE_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.ORANGE_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.MAGENTA_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.LIGHT_BLUE_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.YELLOW_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.LIME_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.PINK_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.GRAY_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.LIGHT_GRAY_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.CYAN_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.PURPLE_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.BLUE_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.BROWN_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.GREEN_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.RED_GLAZED_TERRACOTTA_SLAB.get(), ESPSlabs.BLACK_GLAZED_TERRACOTTA_SLAB.get(),
-				ESPSlabs.PACKED_MUD_SLAB.get(), ESPStairs.TUFF_STAIRS.get(),
-				ESPStairs.CALCITE_STAIRS.get(), ESPStairs.DRIPSTONE_STAIRS.get(),
-				ESPStairs.END_STONE_STAIRS.get(), ESPStairs.NETHERRACK_STAIRS.get(),
-				ESPStairs.PACKED_MUD_STAIRS.get(), ESPCorners.STONE_CORNER.get(),
-				ESPCorners.COBBLESTONE_CORNER.get(), ESPCorners.STONE_BRICK_CORNER.get(),
-				ESPCorners.ANDESITE_CORNER.get(), ESPCorners.POLISHED_ANDESITE_CORNER.get(),
-				ESPCorners.DIORITE_CORNER.get(), ESPCorners.POLISHED_DIORITE_CORNER.get(),
-				ESPCorners.GRANITE_CORNER.get(), ESPCorners.POLISHED_DIORITE_CORNER.get(),
-				ESPCorners.TUFF_CORNER.get(), ESPCorners.CALCITE_CORNER.get(),
-				ESPCorners.DRIPSTONE_CORNER.get(), ESPCorners.BLACKSTONE_CORNER.get(),
-				ESPCorners.POLISHED_BLACKSTONE_CORNER.get(), ESPCorners.POLISHED_BLACKSTONE_BRICK_CORNER.get(),
-				ESPCorners.NETHERRACK_CORNER.get(), ESPCorners.NETHER_BRICK_CORNER.get(),
-				ESPCorners.RED_NETHER_BRICK_CORNER.get(), ESPCorners.END_STONE_CORNER.get(),
-				ESPCorners.END_STONE_BRICK_CORNER.get(), ESPCorners.PURPUR_CORNER.get(),
-				ESPCorners.PACKED_MUD_CORNER.get(), ESPCorners.MUD_BRICK_CORNER.get());
 	}
 }
