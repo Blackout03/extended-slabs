eced88cab09a611c131bdc6add1b8afd7dfe1482
Blackout <gardner.hayden03@gmail.com>
2026-06-27 16:52:48 +1000

    Add common platform registry helpers

---
+++ common/src/main/java/com/blackout/extendedslabs/platform/PlatformRegistry.java
@@ -0,0 +1,10 @@
+package com.blackout.extendedslabs.platform;
+
+import net.minecraft.core.Registry;
+import net.minecraft.resources.ResourceKey;
+
+public class PlatformRegistry {
+	public static <T> ModRegistry<T> create(ResourceKey<? extends Registry<T>> registryKey) {
+		return new ModRegistry<>(registryKey);
+	}
+}
